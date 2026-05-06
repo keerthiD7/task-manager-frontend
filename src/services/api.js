3472ece39a473748d58e7da5358dcd7738ef8480
keerthi <keerthidarsi04@gmail.com>
2026-05-06 19:03:06 +0530

    updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://task-manager-system-production-4b32.up.railway.app"
+    baseURL: "http://localhost:8080"
 });
 
 export default API;
